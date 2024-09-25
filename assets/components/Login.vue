e80b058e8c05477748bcf00376b930d4a1bf9b87
Victor Alfonso <vc70383@hotmail.com>
2024-09-25 17:51:49 -0600

    some changes and login controller

---
+++ assets/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="login">
+      <h2>Login</h2>
+      <form @submit.prevent="submitLogin">
+        <div>
+          <label for="username">Username</label>
+          <input v-model="username" type="text" id="username" />
+        </div>
+        <div>
+          <label for="password">Password</label>
+          <input v-model="password" type="password" id="password" />
+        </div>
+        <button type="submit">Login</button>
+        <p v-if="error">{{ error }}</p>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  export default {
+    data() {
+      return {
+        username: "",
+        password: "",
+        error: null,
+      };
+    },
+    methods: {
+      submitLogin() {
+        axios
+          .post("/login", {
+            username: this.username,
+            password: this.password,
+          })
+          .then((response) => {
+            // Redirigir o manejar login exitoso
+            window.location.href = "/dashboard"; // Ejemplo
+          })
+          .catch((error) => {
+            this.error = "Login failed. Please check your credentials.";
+          });
+      },
+    },
+  };
+  </script>
+  
---
+++ src/Controller/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Obtener el error de login, si existe
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Último nombre de usuario ingresado
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Symfony manejará el logout
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+}
