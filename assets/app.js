34278a0fee4c0781273ccc5c1e2828b42591fbae
Victor Alfonso <vc70383@hotmail.com>
2024-09-25 14:12:49 -0600

    configs and install axios

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+createApp(App)
+  .use(router)
+  .mount('#app');
